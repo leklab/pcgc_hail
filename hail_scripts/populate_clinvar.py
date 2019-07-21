90f923c57bd563ae154f2c7f61f8fbe484cf4071
Monkol Lek <monkol.lek@yale.edu>
2019-07-21 18:57:49 -0500

    Refactored populate clinvar code

--- hail_scripts/populate_clinvar.py
+++ hail_scripts/populate_clinvar.py
@@ -1,8 +1,11 @@
 import argparse
+import gzip
+import logging
 
 import hail as hl
 
-from utils.clinvar import CLINVAR_GOLD_STARS_LOOKUP, download_and_import_latest_clinvar_vcf
+#from utils.clinvar import CLINVAR_GOLD_STARS_LOOKUP, download_and_import_latest_clinvar_vcf
+
 from utils import (
     get_expr_for_alt_allele,
     get_expr_for_contig,
@@ -18,9 +21,48 @@ from utils import (
     get_expr_for_vep_transcript_id_to_consequence_map,
     get_expr_for_vep_transcript_ids_set,
     get_expr_for_worst_transcript_consequence_annotations_struct,
+    get_expr_for_variant_ids,
 )
 
 from export_ht_to_es import *
+
+logger = logging.getLogger()
+
+CLINVAR_GOLD_STARS_LOOKUP = hl.dict(
+    {
+        "no_interpretation_for_the_single_variant": 0,
+        "no_assertion_provided": 0,
+        "no_assertion_criteria_provided": 0,
+        "criteria_provided,_single_submitter": 1,
+        "criteria_provided,_conflicting_interpretations": 1,
+        "criteria_provided,_multiple_submitters,_no_conflicts": 2,
+        "reviewed_by_expert_panel": 3,
+        "practice_guideline": 4,
+    }
+)
+
+
+
+def _parse_clinvar_release_date(local_vcf_path: str) -> str:
+    """Parse clinvar release date from the VCF header.
+
+    Args:
+        local_vcf_path (str): clinvar vcf path on the local file system.
+
+    Returns:
+        str: return VCF release date as string, or None if release date not found in header.
+    """
+    with gzip.open(local_vcf_path, "rt") as f:
+        for line in f:
+            if line.startswith("##fileDate="):
+                clinvar_release_date = line.split("=")[-1].strip()
+                return clinvar_release_date
+
+            if not line.startswith("#"):
+                return None
+
+    return None
+
 
 '''
 p = argparse.ArgumentParser()
@@ -40,76 +82,130 @@ else:
     index_name = "clinvar_grch{}".format(args.genome_version)
 '''
 
+def import_vcf(
+        vcf_path: str,
+        genome_version: str,
+        min_partitions: int = None,
+        force_bgz: bool = True,
+        drop_samples: bool = False,
+        skip_invalid_loci: bool = False,
+        split_multi_alleles: bool = True):
+    """Import vcf and return MatrixTable.
 
-print("\n=== Downloading VCF ===")
-mt = download_and_import_latest_clinvar_vcf("37")
-#print(dict(mt.globals.values))
+    :param str vcf_path: MT to annotate with VEP
+    :param str genome_version: "37" or "38"
+    :param int min_partitions: min partitions
+    :param bool force_bgz: read .gz as a bgzipped file
+    :param bool drop_samples: if True, discard genotype info
+    :param bool skip_invalid_loci: if True, skip loci that are not consistent with the reference_genome.
+    """
 
+    if genome_version not in ("37", "38"):
+        raise ValueError(f"Invalid genome_version: {genome_version}")
 
+    logger.info(f"\n==> import vcf: {vcf_path}")
 
-print("\n=== Running VEP ===")
-mt = hl.vep(mt, 'vep85-loftee-local.json', name="vep")
+    # add (or remove) "chr" prefix from vcf chroms so they match the reference
+    ref = hl.get_reference(f"GRCh{genome_version}")
+    contig_recoding = {
+        **{ref_contig.replace("chr", ""): ref_contig for ref_contig in ref.contigs if "chr" in ref_contig},
+        **{f"chr{ref_contig}": ref_contig for ref_contig in ref.contigs if "chr" not in ref_contig}}
 
-print("\n=== Processing ===")
-mt = mt.annotate_rows(
-    sortedTranscriptConsequences=get_expr_for_vep_sorted_transcript_consequences_array(vep_root=mt.vep)
-)
+    mt = hl.import_vcf(
+        vcf_path,
+        reference_genome=f"GRCh{genome_version}",
+        contig_recoding=contig_recoding,
+        min_partitions=min_partitions,
+        force_bgz=force_bgz,
+        drop_samples=drop_samples,
+        skip_invalid_loci=skip_invalid_loci)
 
-mt = mt.annotate_rows(
-    main_transcript=get_expr_for_worst_transcript_consequence_annotations_struct(
-        vep_sorted_transcript_consequences_root=mt.sortedTranscriptConsequences
+    mt = mt.annotate_globals(sourceFilePath=vcf_path, genomeVersion=genome_version)
+
+    mt = mt.annotate_rows(
+        original_alt_alleles=hl.or_missing(hl.len(mt.alleles) > 2, get_expr_for_variant_ids(mt.locus, mt.alleles))
     )
-)
 
-mt = mt.annotate_rows(
-    gene_ids=get_expr_for_vep_gene_ids_set(
-        vep_transcript_consequences_root=mt.sortedTranscriptConsequences
-    ),
-)
+    if split_multi_alleles:
+        mt = hl.split_multi_hts(mt)
+        mt = mt.key_rows_by(**hl.min_rep(mt.locus, mt.alleles))
 
-review_status_str = hl.delimit(hl.sorted(hl.array(hl.set(mt.info.CLNREVSTAT)), key=lambda s: s.replace("^_", "z")))
-
-mt = mt.select_rows(
-    allele_id=mt.info.ALLELEID,
-    alt=get_expr_for_alt_allele(mt),
-    chrom=get_expr_for_contig(mt.locus),
-    clinical_significance=hl.delimit(hl.sorted(hl.array(hl.set(mt.info.CLNSIG)), key=lambda s: s.replace("^_", "z"))),
-    domains=get_expr_for_vep_protein_domains_set(vep_transcript_consequences_root=mt.vep.transcript_consequences),
-    gene_ids=mt.gene_ids,
-    gene_id_to_consequence_json=get_expr_for_vep_gene_id_to_consequence_map(
-        vep_sorted_transcript_consequences_root=mt.sortedTranscriptConsequences,
-        gene_ids=mt.gene_ids
-    ),
-    gold_stars=CLINVAR_GOLD_STARS_LOOKUP[review_status_str],
-    **{f"main_transcript_{field}": mt.main_transcript[field] for field in mt.main_transcript.dtype.fields},
-    pos=get_expr_for_start_pos(mt),
-    ref=get_expr_for_ref_allele(mt),
-    review_status=review_status_str,
-    transcript_consequence_terms=get_expr_for_vep_consequence_terms_set(
-        vep_transcript_consequences_root=mt.sortedTranscriptConsequences
-    ),
-    transcript_ids=get_expr_for_vep_transcript_ids_set(
-        vep_transcript_consequences_root=mt.sortedTranscriptConsequences
-    ),
-    transcript_id_to_consequence_json=get_expr_for_vep_transcript_id_to_consequence_map(
-        vep_transcript_consequences_root=mt.sortedTranscriptConsequences
-    ),
-    variant_id=get_expr_for_variant_id(mt),
-    xpos=get_expr_for_xpos(mt.locus),
-)
-
-#print("\n=== Summary ===")
-#hl.summarize_variants(mt)
+    return mt
 
 
-# Drop key columns for export
-rows = mt.rows()
-rows = rows.order_by(rows.variant_id).drop("locus", "alleles")
-rows.write('clinvar.ht',overwrite=True)
 
-#print("\n=== Exporting to Elasticsearch ===")
-#es = ElasticsearchClient(args.host, args.port)
-#export_ht_to_es(rows, index_name = 'clinvar_grch37',index_type = 'variant')
+def populate_clinvar():
+
+    clinvar_release_date = _parse_clinvar_release_date('clinvar.vcf.gz')
+    #mt = import_vcf(clinvar_vcf_hdfs_path, genome_version, drop_samples=True, min_partitions=2000, skip_invalid_loci=True)
+    mt = import_vcf('clinvar.vcf.gz', "37", drop_samples=True, min_partitions=2000, skip_invalid_loci=True)
+    mt = mt.annotate_globals(version=clinvar_release_date)
+
+
+    '''
+    print("\n=== Running VEP ===")
+    mt = hl.vep(mt, 'vep85-loftee-local.json', name="vep")
+
+    print("\n=== Processing ===")
+    mt = mt.annotate_rows(
+        sortedTranscriptConsequences=get_expr_for_vep_sorted_transcript_consequences_array(vep_root=mt.vep)
+    )
+
+    mt = mt.annotate_rows(
+        main_transcript=get_expr_for_worst_transcript_consequence_annotations_struct(
+            vep_sorted_transcript_consequences_root=mt.sortedTranscriptConsequences
+        )
+    )
+
+    mt = mt.annotate_rows(
+        gene_ids=get_expr_for_vep_gene_ids_set(
+            vep_transcript_consequences_root=mt.sortedTranscriptConsequences
+        ),
+    )
+
+    review_status_str = hl.delimit(hl.sorted(hl.array(hl.set(mt.info.CLNREVSTAT)), key=lambda s: s.replace("^_", "z")))
+
+    mt = mt.select_rows(
+        allele_id=mt.info.ALLELEID,
+        alt=get_expr_for_alt_allele(mt),
+        chrom=get_expr_for_contig(mt.locus),
+        clinical_significance=hl.delimit(hl.sorted(hl.array(hl.set(mt.info.CLNSIG)), key=lambda s: s.replace("^_", "z"))),
+        domains=get_expr_for_vep_protein_domains_set(vep_transcript_consequences_root=mt.vep.transcript_consequences),
+        gene_ids=mt.gene_ids,
+        gene_id_to_consequence_json=get_expr_for_vep_gene_id_to_consequence_map(
+            vep_sorted_transcript_consequences_root=mt.sortedTranscriptConsequences,
+            gene_ids=mt.gene_ids
+        ),
+        gold_stars=CLINVAR_GOLD_STARS_LOOKUP[review_status_str],
+        **{f"main_transcript_{field}": mt.main_transcript[field] for field in mt.main_transcript.dtype.fields},
+        pos=get_expr_for_start_pos(mt),
+        ref=get_expr_for_ref_allele(mt),
+        review_status=review_status_str,
+        transcript_consequence_terms=get_expr_for_vep_consequence_terms_set(
+            vep_transcript_consequences_root=mt.sortedTranscriptConsequences
+        ),
+        transcript_ids=get_expr_for_vep_transcript_ids_set(
+            vep_transcript_consequences_root=mt.sortedTranscriptConsequences
+        ),
+        transcript_id_to_consequence_json=get_expr_for_vep_transcript_id_to_consequence_map(
+            vep_transcript_consequences_root=mt.sortedTranscriptConsequences
+        ),
+        variant_id=get_expr_for_variant_id(mt),
+        xpos=get_expr_for_xpos(mt.locus),
+    )
+
+    #print("\n=== Summary ===")
+    #hl.summarize_variants(mt)
+
+
+    # Drop key columns for export
+    rows = mt.rows()
+    rows = rows.order_by(rows.variant_id).drop("locus", "alleles")
+    rows.write('clinvar.ht',overwrite=True)
+    '''
+    #print("\n=== Exporting to Elasticsearch ===")
+    #es = ElasticsearchClient(args.host, args.port)
+    #export_ht_to_es(rows, index_name = 'clinvar_grch37',index_type = 'variant')
 
 '''
 es.export_table_to_elasticsearch(
@@ -123,3 +219,8 @@ es.export_table_to_elasticsearch(
     verbose=True,
 )
 '''
+
+
+if __name__ == "__main__":
+    hl.init()
+    populate_clinvar()
